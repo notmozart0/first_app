f2fab77be42b1663bdb4fdcfbb9241e915af0182
notmozart0 <notmozart0@gmail.com>
2026-05-15 22:18:48 +0300

    Add files via upload

--- features/server.js
+++ features/server.js
@@ -1,1 +1,14 @@
+const express = require ("express");
+const app = express();
+app.use(express.json());
 
+app.use("/auth" , require ("./auth_and_users/auth_routes.js"));
+//app.use("/locals" , require "./locals/locals_routes.js")
+//app.use("/places" , require "./places/places_routes.js")
+
+
+
+
+
+
+app.listen (3000, ()=> console.log ("server has booted succesfully"));
