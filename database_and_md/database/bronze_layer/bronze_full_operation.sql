92d55b10be405b4eebab436aa8305401b8060d08
yousef mohamed <y.abdalla2611@gmail.com>
2026-05-16 18:06:48 +0300

    Update bronze_full_operation.sql

--- database_and_md/database/bronze_layer/bronze_full_operation.sql
+++ database_and_md/database/bronze_layer/bronze_full_operation.sql
@@ -6,8 +6,8 @@ it dosen't operate any data transaction  or cleanig (at least in this phase).
 =======================================================================================
                        instruction to operate the code properly:
 =======================================================================================
-to run the code properly you must run the entier code only once and if u want to load the 
-tables agine you must run the next code: 
+to run the code properly you must run the entier code only once and if you want to load the 
+tables agine you must only run the next code: 
 
 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> (EXEC bronze.treating_tables;) <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< 
 
@@ -49,7 +49,20 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 		SET @start_time = GETDATE();
 			PRINT '>> treating users table <<';
 
-			IF OBJECT_ID ('bronze.users', 'U') IS NOT NULL 
+			IF OBJECT_ID ('bronze.users', 'U') IS NULL 
+			BEGIN
+			CREATE TABLE bronze.users (
+				user_id INT IDENTITY(1,1) PRIMARY KEY,
+				user_email NVARCHAR (MAX),
+				user_role NVARCHAR (MAX),
+				user_password NVARCHAR (70),
+				first_name NVARCHAR (50),
+				last_name NVARCHAR (50),
+				phone_number NVARCHAR(15) ,
+				profile_picture VARBINARY(MAX),
+				user_create_date DATETIME2 DEFAULT GETDATE()
+				);
+			END 
 					BULK INSERT bronze.users 
 					FROM 'D:\projects\toursim app\database\users.csv'
 					WITH (
@@ -59,16 +72,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 						TABLOCK,
 						KEEPIDENTITY
 					);
-			ELSE 
-			CREATE TABLE bronze.users (
-				user_id INT IDENTITY(1,1) PRIMARY KEY,
-				password NVARCHAR (70),
-				first_name NVARCHAR (50),
-				last_name NVARCHAR (50),
-				phone_number NVARCHAR(15) ,
-				profile_picture VARBINARY(MAX),
-				user_create_date DATETIME2 DEFAULT GETDATE()
-				);
+
 				SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> ------------------------';
@@ -77,17 +81,8 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 
 			SET @start_time = GETDATE();
 			PRINT '>> treating places table <<';
-			IF OBJECT_ID ('bronze.places', 'U') IS NOT NULL
-					BULK INSERT bronze.places
-					FROM 'D:\projects\toursim app\database\places.csv'
-					WITH (
-						FIRSTROW = 2,
-						FIELDTERMINATOR = ',',
-						ROWTERMINATOR = '\n',
-						TABLOCK,
-						KEEPIDENTITY
-					);
-			ELSE 
+			IF OBJECT_ID ('bronze.places', 'U') IS NULL
+			BEGIN
 			CREATE TABLE bronze.places (
 				place_id INT IDENTITY(1,1) PRIMARY KEY,
 				place_name NVARCHAR (70),
@@ -99,6 +94,17 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 				opening_hours NVARCHAR (MAX),
 				place_location NVARCHAR (MAX)
 				);
+			END 
+					BULK INSERT bronze.places
+					FROM 'D:\projects\toursim app\database\places.csv'
+					WITH (
+						FIRSTROW = 2,
+						FIELDTERMINATOR = ',',
+						ROWTERMINATOR = '\n',
+						TABLOCK,
+						KEEPIDENTITY
+					);
+
 				SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -----------------------';
@@ -107,7 +113,17 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 
 			SET @start_time = GETDATE();
 			PRINT '>> treating reviwes table <<';
-			IF OBJECT_ID ('bronze.reviwes', 'U') IS NOT NULL 
+			IF OBJECT_ID ('bronze.reviwes', 'U') IS NULL
+			BEGIN
+			CREATE TABLE bronze.reviwes (
+				reviwe_id INT IDENTITY(1,1) PRIMARY KEY,
+				place_id INT,
+				local_id INT,
+				rateing DECIMAL(3, 2),
+				reviwe_text NVARCHAR (MAX),
+				reviwe_date DATETIME2 DEFAULT GETDATE()
+				);
+			END 
 					BULK INSERT bronze.reviwes 
 					FROM 'D:\projects\toursim app\database\reviwes.csv'
 					WITH (
@@ -117,15 +133,6 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 						TABLOCK,
 						KEEPIDENTITY
 					);
-			ELSE 
-			CREATE TABLE bronze.reviwes (
-				reviwe_id INT IDENTITY(1,1) PRIMARY KEY,
-				place_id INT,
-				local_id INT,
-				rateing DECIMAL(3, 2),
-				reviwe_text NVARCHAR (MAX),
-				reviwe_date DATETIME2 DEFAULT GETDATE()
-				);
 				SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> ---------------------------';
@@ -134,17 +141,8 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 
 			SET @start_time = GETDATE();
 			PRINT '>> treating locals table <<';
-			IF OBJECT_ID ('bronze.locals', 'U') IS NOT NULL 
-					BULK INSERT bronze.locals 
-					FROM 'D:\projects\toursim app\database\locals.csv'
-					WITH (
-						FIRSTROW = 2,
-						FIELDTERMINATOR = ',',
-						ROWTERMINATOR = '\n',
-						TABLOCK,
-						KEEPIDENTITY
-					);
-			ELSE 
+			IF OBJECT_ID ('bronze.locals', 'U') IS NULL 
+			BEGIN
 			CREATE TABLE bronze.locals (
 				local_id INT IDENTITY(1,1) PRIMARY KEY,
 				first_name NVARCHAR (50),
@@ -154,6 +152,17 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 				discrption NVARCHAR (MAX) ,
 				local_photo VARBINARY(MAX)
 				);
+			END 
+					BULK INSERT bronze.locals 
+					FROM 'D:\projects\toursim app\database\locals.csv'
+					WITH (
+						FIRSTROW = 2,
+						FIELDTERMINATOR = ',',
+						ROWTERMINATOR = '\n',
+						TABLOCK,
+						KEEPIDENTITY
+					);
+
 				SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> -----------------------';
@@ -162,7 +171,15 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 
 			SET @start_time = GETDATE();
 			PRINT '>> treating media table <<';
-			IF OBJECT_ID ('bronze.media', 'U') IS NOT NULL 
+			IF OBJECT_ID ('bronze.media', 'U') IS NULL 
+			BEGIN
+			CREATE TABLE bronze.media (
+				photo_id INT IDENTITY(1,1) PRIMARY KEY,
+				place_id INT,
+				photo_url NVARCHAR (MAX),
+				caption NVARCHAR (MAX)
+				);
+			END 
 					BULK INSERT bronze.media 
 					FROM 'D:\projects\toursim app\database\media.csv'
 					WITH (
@@ -172,14 +189,7 @@ DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @b
 						TABLOCK,
 						KEEPIDENTITY
 					);
-			ELSE 
 
-			CREATE TABLE bronze.media (
-				photo_id INT IDENTITY(1,1) PRIMARY KEY,
-				place_id INT,
-				photo_url NVARCHAR (MAX),
-				caption NVARCHAR (MAX)
-				);
 				SET @end_time = GETDATE();
 		PRINT '>> Load Duration: ' + CAST(DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
 		PRINT '>> ------------------------';
