522d5dfaf26f6f930f17993bf4161671dd4b14b4
Arwamohamed2512 <arwamo2612@gmail.com>
2026-05-18 00:08:13 +0300

    Add files via upload

---
+++ features/reviews/reviews/reviews_data.js
@@ -0,0 +1,23 @@
+// fake data to test
+let reviews=[
+    {
+        review_id:1,
+        place_id:10,
+        local_id:1,
+        rating:4,
+        review_text:'Great place to visit!',
+        review_date: '2023-01-01'
+    },
+    {
+        review_id:2,
+        place_id:101,
+        local_id:2,
+        rating:5,
+        review_text:'Great place to visit!',
+        review_date: '2023-01-02'
+    }
+]
+
+module.exports = {
+    reviews
+};
